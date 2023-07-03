f69b53cc44a69046935b0d8d07dcfe6fb1c8392b
soxhwhat <1246669436@qq.com>
2023-07-03 11:49:58 +0800

    docs: 工厂模式
    1.克服简单工程模式破坏开闭原则的缺点
    2.通过实现抽象接口、多态等特性实现够工厂模式

--- src/designpattern/factory/method/FactoryClient.java
+++ src/designpattern/factory/method/FactoryClient.java
@@ -4,7 +4,9 @@ import designpattern.factory.simple.Operation;
 
 /**
  * 工厂方法客户端
- * 
+ *
+ *
+ * 由于使用了多态性，工厂方法模式保持了简单工厂模式的优点，而且克服了它的缺点。但缺点是由于每加一个产品，就需要加一个产品工厂的类，增加了额外的开发量。
  * @author liu yuning
  *
  */
